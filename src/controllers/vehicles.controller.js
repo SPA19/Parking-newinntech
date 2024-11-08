d051b8242a0c3df1c0a589b43980109d584299fe
SPA19 <simon.150@hotmail.com>
2024-11-07 23:24:55 -0500

    Updated vehicles controller and routes to add new endpoints and functionality

--- src/controllers/vehicles.controller.js
+++ src/controllers/vehicles.controller.js
@@ -1,18 +1,44 @@
 import Vehicle from "../models/vehicles.model.js";
 
 const vehicleController = {
-  getAllVehicle: async (req, res) => {
+  getAllVehicles: async (req, res) => {
     try {
-      const vehicle = await Vehicle.find();
-      res.status(200).json(vehicle);
+      const response = await Vehicle.find();
+      res.status(200).json(response);
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  },
+
+  getVehicleById: async (req, res) => {
+    try {
+      const response = await Vehicle.findById(req.params.id);
+      if (!response)
+        return res.status(404).json({ message: "Vehiculo no encontrado" });
+      res.status(200).json(response);
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  },
+
+  getVehicleByPlate: async (req, res) => {
+    try {
+      const { plate } = req.params;
+      const response = await Vehicle.find({ plate });
+
+      if (response.length === 0)
+        return res.status(404).json({
+          message: `Vehículo con placa ${plate} no fue encontrado`,
+        });
+      res.status(200).json(response);
     } catch (err) {
       res.status(500).json({ message: err.message });
     }
   },
 
   createNewVehicle: async (req, res) => {
-    const { plate, type, entryTime } = req.body;
     try {
+      const { plate, type, entryTime } = req.body;
       //validar si la placa ya esta registrada y el vehiculo sigue en el parqueadero
       const existingPlate = await Vehicle.findOne({
         plate,
@@ -20,9 +46,9 @@ const vehicleController = {
       });
 
       if (existingPlate) {
-        return res
-          .status(400)
-          .json({ message: `El vehiculo con placa ${plate} se encuentra todavia en el parqueadero`});
+        return res.status(400).json({
+          message: `El vehiculo con placa ${plate} se encuentra todavia en el parqueadero`,
+        });
       }
       //cuenta el numero de carros que hay en el momento teniendo en cuenta que no tenga hora de salida
       const existingCars = await Vehicle.countDocuments({
@@ -56,6 +82,35 @@ const vehicleController = {
     } catch (err) {
       res.status(500).json({ message: err.message });
     }
+  },
+
+  updateVehicle: async (req, res) => {
+    const currentDate = new Date();
+    const response = await Vehicle.findByIdAndUpdate(
+      req.params.id,
+      { exitTime: currentDate },
+      {
+        new: true,
+      }
+    );
+    if (!response)
+      return res.status(404).json({ message: "Vehiculo no encontrado" });
+
+    res.status(200).json({
+      message: "Hora de salida registrada correctamente",
+      data: response,
+    });
+  },
+
+  deleteVehicle: async (req, res) => {
+    const response = await Vehicle.findByIdAndDelete(req.params.id);
+    if (!response)
+      return res.status(404).json({ message: "Vehiculo no encontrado" });
+
+    res.status(200).json({
+      message: "Se elimino correctamente",
+      data: response,
+    });
   },
 };
 
--- src/routes/vehicles.routes.js
+++ src/routes/vehicles.routes.js
@@ -3,8 +3,16 @@ import vehicleController from "../controllers/vehicles.controller.js";
 
 const router = Router();
 
-router.get("/allVehicles", vehicleController.getAllVehicle);
+router.get("/allVehicles", vehicleController.getAllVehicles);
+
+router.get("/vehicles/:id", vehicleController.getVehicleById);
+
+router.get("/plateVehicles/:plate", vehicleController.getVehicleByPlate);
 
 router.post("/createVehicles", vehicleController.createNewVehicle);
+
+router.put("/updVehicles/:id", vehicleController.updateVehicle);
+
+router.delete("/vehicles/:id", vehicleController.deleteVehicle);
 
 export default router;
